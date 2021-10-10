a27c00e6f6d13ebfdb1f6a72da52584be53bc35f
Cerberus270 <geoavila718@gmail.com>
2021-10-10 16:26:37 -0600

    llenando select con paises

--- js/about.js
+++ js/about.js
@@ -3,6 +3,9 @@ import CalidadAire from './calidadAire.js';
 import OpenWeather from './openWeather.js';
 
 UI.formularioAire.addEventListener('submit', buscarCalidadAire);
+UI.formularioClima.addEventListener('submit', buscarClima);
+
+document.addEventListener('DOMContentLoaded', UI.llenarPaises(UI.cmbPaises));
 
 function buscarCalidadAire(e){
     e.preventDefault();
@@ -18,6 +21,17 @@ function buscarCalidadAire(e){
     }
 }
 
-const busquedaClima = new OpenWeather('Armenia','SV');
-
-busquedaClima.consultarAPI();
+function buscarClima(e){
+    e.preventDefault();
+    const ciudad= UI.txtCiudadClima.value;
+    const pais = UI.cmbPaises.value;
+    if (ciudad === '') {
+        console.error('La ciudad es obligatoria');
+        return;
+    } else {
+        console.log(ciudad);
+        console.log(pais);
+        const busquedaClima = new OpenWeather(ciudad,pais);
+        busquedaClima.consultarAPI();
+    }
+}
--- js/selectores.js
+++ js/selectores.js
@@ -5,7 +5,10 @@ export const formularioAire = document.querySelector('#formularioAire'),
     mapaAire = document.querySelector('#mapaAire');
 
 //Selectores Clima
-export const mapaClima = document.querySelector('#mapaClima');
+export const mapaClima = document.querySelector('#mapaClima'),
+       cmbPaises = document.querySelector('#cmbPaises'),
+       txtCiudadClima = document.querySelector('#txtCiudadClima'),
+       formularioClima = document.querySelector('#formularioClima');
 
 export function alert(message, type) {
     var alerta = document.createElement('div')
@@ -15,20 +18,37 @@ export function alert(message, type) {
 
 export function limpiarChildrens(parent) {
     parent.removeAttribute("class");
-    while(parent.firstChild) {
+    while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-export function spinner(parent){
+export function spinner(parent) {
     limpiarChildrens(parent);
-    parent.setAttribute('class','text-center');
+    parent.setAttribute('class', 'text-center');
     const spin = document.createElement('div');
     spin.classList.add('spinner-grow');
-    spin.setAttribute('role','status');
+    spin.setAttribute('role', 'status');
     const accesibilidad = document.createElement('span');
     accesibilidad.textContent = 'Loading...';
-    accesibilidad.setAttribute('class','visually-hidden');
+    accesibilidad.setAttribute('class', 'visually-hidden');
     spin.appendChild(accesibilidad);
     parent.appendChild(spin);
+}
+
+export function llenarPaises(parent) {
+    const url = 'https://restcountries.com/v2/all';
+    fetch(url).then(res => {
+        return res.json();
+    }).then(data => {
+        let paises = "";
+        data.forEach(country => {
+            paises += `
+      <option value="${country.alpha2Code}">${country.name}</option>`;
+        })
+
+        parent.innerHTML = paises;
+    }).catch(err => {
+        console.log(err);
+    })
 }
