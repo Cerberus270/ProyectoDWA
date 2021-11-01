a57298153aed4043257867987f5a0325a34689f4
Cerberus270 <geoavila718@gmail.com>
2021-11-01 12:05:12 -0600

    Agregando loader mientras se envia el correo - feat(correo.js)

--- js/correo.js
+++ js/correo.js
@@ -3,19 +3,21 @@ const formulario = document.getElementById('formularioContacto');
 formulario.addEventListener('submit', enviarCorreo);
 
 function enviarCorreo(e) {
+   Swal.fire('Por favor espere mientras se envia el correo');
+   Swal.showLoading()
    e.preventDefault();
    emailjs.sendForm('service_prmtfs5', 'template_ry0eoo9', '#formularioContacto')
-   .then(response => {
-       console.log('SUCCESS!', response.status, response.text);
-      //Sweet Alert
-      Swal.fire({
-         title: 'Excelente',
-         text: 'Datos Enviados Correctamente',
-         icon: 'success',
+      .then(response => {
+         console.log('SUCCESS!', response.status, response.text);
+         //Sweet Alert
+         Swal.fire({
+            title: 'Excelente',
+            text: 'Datos Enviados Correctamente',
+            icon: 'success'
+         });
+         //Reset formulario
+         //formulario.reset()
+      }, error => {
+         console.log('FAILED...', error);
       });
-      //Reset formulario
-      //formulario.reset()
-   }, error => {
-       console.log('FAILED...', error);
-   });
 }
