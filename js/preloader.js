472d40b94d2841f2aab1d6e1ce03563f61ccd3d1
Cerberus270 <geoavila718@gmail.com>
2021-11-01 18:22:30 -0600

    Agregando animaciones con gsap

--- js/preloader.js
+++ js/preloader.js
@@ -1,4 +1,17 @@
 $(window).on('load',function () {
     // Animaton when load is complete
     $(".loader").fadeOut("slow");
+    let tl = gsap.timeline({
+
+    })
+    tl.fromTo("#apiFondo", {
+        opacity: 0,
+        y: -200,
+    }, {
+        delay: 0.5,
+        y: 0,
+        opacity: 1,
+        duration: 0.8,
+        ease: 'bounce',
+    });
 });
